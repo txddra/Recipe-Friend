394582895355571b3d987b269d8ca50fbccc650e
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-17 17:15:00 -0600

    made publisher visiable

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -8,15 +8,18 @@ export const Recipes = (props) => {
         <div className="row">
         {props.recipes.map((recipe)=>{
           return (
-            <div key ={recipe.recipe.label} className = "col-md-4">
+            <div key ={recipe.recipe.label} className = "col-md-4" style ={{marginBottom:"2rem"}}>
                 <div className ="recipe__box">
             <img
             className="recipe__box-img" 
             src={recipe.recipe.image} 
             alt={recipe.recipe.label}/>
             <div className="recipe__text">
-                
-                <h5>{recipe.recipe.label}</h5>
+
+                <h5 className ="recipes__title">{recipe.recipe.label}</h5>
+                <p className ="recipes__subtitle">Publisher:</p>
+                <span>{recipe.recipe.source}</span>
+
             </div>
                 </div>
            </div>

f4ec8e1a9988f8933bf37a96bbcc9c02b51d7b17
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-17 17:32:37 -0600

    overall update

---
+++ src/components/Router.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import App from "../App"
+
+const Router =()=>{
+  <BrowserRouter>
+  <Route exact path ='/' component ={}/>
+  </BrowserRouter>
+}

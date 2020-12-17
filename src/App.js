f5546c646be78b6ab60e07a025328a59c2e0af90
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-17 14:20:38 -0600

    data returned from api

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import './App.css';
 
 import RecipeForm from './components/RecipeForm'
 
+const API_KEY1 =  `b87d0857`
+const API_KEY2 = `11bf59d067701580d54a3c72b5bd2161`;
+// const chicken = ;
+
+
 
 class App extends Component{
 
   //makes the call to the recipe api
 
-  recipeGrab=(e)=>{
-    e.preventDefault();
+  recipeGrab= async(e)=>{
+    //logs out what is typed into the input field
     const recipeName = e.target.elements.recipeName.value;
+    //prevents automatic page refresh
+    e.preventDefault();
 
-    console.log(recipeName)
+    const response = await fetch(
+`https://api.edamam.com/search?q=chicken&app_id=${API_KEY1}&app_key=${API_KEY2}&from=0&to=3&calories=591-722&health=alcohol-free`)
+
+
+const results = await response.json();
+console.log(results)
   }
 
   render(){

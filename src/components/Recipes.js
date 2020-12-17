0bdf45c311120d63d5814cdff8f232912f3845d4
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-17 17:24:34 -0600

    trimmed the strings of the title

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -16,7 +16,10 @@ export const Recipes = (props) => {
             alt={recipe.recipe.label}/>
             <div className="recipe__text">
 
-                <h5 className ="recipes__title">{recipe.recipe.label}</h5>
+                <h5 className ="recipes__title">
+                    {/* //if the length of the title is less than 20 characters, render the title as is, otherwise trim it to 25 characters */}
+                    {recipe.recipe.label.length < 20 ? `${recipe.recipe.label}` : `${recipe.recipe.label.substring(0,25)}...`}
+                    </h5>
                 <p className ="recipes__subtitle">Publisher:</p>
                 <span>{recipe.recipe.source}</span>
 

ed38638a8ceb4da4c940b764f4f83c1ccf5192d6
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-17 13:41:29 -0600

    added a field

---
+++ src/components/RecipeForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RecipeForm = () =>(
+    <form>
+        <input type="text"></input>
+        <button>Search</button>
+    </form> 
+);
+
+export default RecipeForm;

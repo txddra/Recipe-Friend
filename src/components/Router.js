5a2ec5d226db2122fb0b32fb04e52e324a1012e2
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-17 18:35:21 -0600

    adding switch to Router

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,9 +2,17 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import App from "../App"
+import Recipe from "./Recipe";
 
 const Router =()=>{
   <BrowserRouter>
-  <Route exact path ='/' component ={}/>
+  <Switch>
+  <Route exact path ="/" component ={App} />
+  <Route exact path = "/recipe" component ={Recipe} />
+
+
+  </Switch>
   </BrowserRouter>
 }
+
+export default Router

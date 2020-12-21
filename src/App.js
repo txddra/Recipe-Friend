4631d9a97bccb98263f3986193166b5ccfdefbd8
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 04:44:36 -0600

    real test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import './App.css';
 
-import RecipeForm from './components/RecipeForm';
-import Recipes from './components/Recipes'
+import Search from "./Search/Search"
+// import RecipeForm from './components/RecipeForm';
+// import Recipes from './components/Recipes';
+
 
 const API_KEY1 =   process.env.REACT_APP_RECIPE_KEY_1
 const API_KEY2 =   process.env.REACT_APP_RECIPE_KEY_2
@@ -10,48 +12,58 @@ const API_KEY2 =   process.env.REACT_APP_RECIPE_KEY_2
 
 
 
-class App extends Component{
-
-  state={
-    recipes :[]
-  }
-
-  //makes the call to the recipe api
-
-  recipeGrab= async(e)=>{
-    //logs out what is typed into the input field
-    const recipeName = e.target.elements.recipeName.value;
-    //prevents automatic page refresh
-    e.preventDefault();
-
-    const response = await fetch(
-`https://api.edamam.com/search?q=${recipeName}&count=3&app_id=${API_KEY1}&app_key=${API_KEY2}`)
-
-
-const results = await response.json();
-let recipeArr = results.hits;
-this.setState({recipes: recipeArr})
-// results.map((item)=>{
-  
-// })
-console.log(this.state.recipes)
-  }
-
-  render(){
-    return(
-      <div className ="App">
-        <header className="App-header">
+function App(){
+  return (
+    <div className="App">
+         <header className="App-header">
           <h1 className="app-title"> Recipe Friend</h1>
+          <h3><em>The helpful, handy food finder.</em></h3>
         </header>
-        <RecipeForm recipeGrab = {this.recipeGrab} />
-       <Recipes recipes ={this.state.recipes} />
-            </div>
-           
-          
-
-          )
-        }
-  }
+    </div>
+  )
+}
 
 
 export default App; 
+// function App {
+
+//   state={
+//     recipes :[]
+//   }
+
+//   //makes the call to the recipe api
+
+//   recipeGrab= async(e)=>{
+//     //logs out what is typed into the input field
+//     const recipeName = e.target.elements.recipeName.value;
+//     //prevents automatic page refresh
+//     e.preventDefault();
+
+//     const response = await fetch(
+// `https://api.edamam.com/search?q=${recipeName}&count=3&app_id=${API_KEY1}&app_key=${API_KEY2}`)
+
+
+// const results = await response.json();
+// let recipeArr = results.hits;
+// this.setState({recipes: recipeArr})
+// // results.map((item)=>{
+  
+// // })
+// console.log(this.state.recipes)
+//   }
+
+//   render(){
+//     return(
+//       <div className ="App">
+//         <header className="App-header">
+//           <h1 className="app-title"> Recipe Friend</h1>
+//         </header>
+//         <RecipeForm recipeGrab = {this.recipeGrab} />
+//        <Recipes recipes ={this.state.recipes} />
+//             </div>
+           
+          
+
+//           )
+//         }
+//   }

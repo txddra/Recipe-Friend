96f08c665ff602e5b1c99cbd9f274e99caa40084
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-21 06:00:07 -0600

    added ternery for blank search

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import SearchList from './SearchList'
 
 
-const Search = ({searchValue, recipeFetch}) => {
+const Search = ({searchValue, recipeFetch, recipeResults}) => {
     return (
         <div>
             <form 
                 onChange={(e)=> recipeFetch(e.target.value)} 
-            style={{marginBottom:"2rem"}}>
-                <input 
-                // onChange ={{}}
+                style={{marginBottom:"2rem"}}>
+                
+            <input 
                 style={{marginTop: "2rem"}}
                 className="form__input"
                 type= "text"
                 value ={searchValue}
-                />
-                 <button className='form__button'>Search</button>
+            />
+                <button className='form__button'>Search</button>
             </form>
             
+            {searchValue !== "" && <SearchList recipeResults={recipeResults}/>}
             
         </div>
     )
